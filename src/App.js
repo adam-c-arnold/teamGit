e4b0ffaddf685edcbd4d81b0be49c33dbef9368a
NathanDesk <nathangdev@gmail.com>
2020-05-19 16:23:57 -0400

    nasa fetch

--- src/App.js
+++ src/App.js
@@ -1,36 +1,40 @@
 import React, { Component } from "react";
 //import "./App.css";
-import Nasa from './Nasa';
-import OpenWeather from './OpenWeather';
-import Zomato from './Zomato';
+import Nasa from "./Nasa";
+import OpenWeather from "./OpenWeather";
+import Zomato from "./Zomato";
 
 class App extends Component {
   constructor(props) {
     super(props);
-    this.state = { 
+    this.state = {
       latitude: null,
       longitude: null,
-    }
+    };
   }
+
   componentDidMount() {
+    let location = this;
     navigator.geolocation.getCurrentPosition(function (position) {
       console.log("latititude is:", position.coords.latitude);
       console.log("longititude is:", position.coords.longitude);
-      this.setState ({
+      location.setState({
         latitude: position.coords.latitude,
         longitude: position.coords.longitude,
-      })
+      });
     });
   }
   render() {
     return (
       <div className="App">
         <header className="App-header">
-          <h1>Based on your location, here is some vital information for you!</h1>
+          <h1>
+            Based on your location, here is some vital information for you!
+          </h1>
         </header>
         <Nasa latitude={this.state.latitude} longitude={this.state.longitude} />
-        <OpenWeather latitude={this.state.latitude} longitude={this.state.longitude} />
-        <Zomato latitude={this.state.latitude} longitude={this.state.longitude} />
+        {/* <OpenWeather latitude={this.state.latitude} longitude={this.state.longitude} /> */}
+        {/* <Zomato latitude={this.state.latitude} longitude={this.state.longitude} /> */}
       </div>
     );
   }
