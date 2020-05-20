92604b1ea85bf2f6df8b8e62f53d0a83e0d073c9
Nathan Garrett <nathangdev@gmail.com>
2020-05-20 11:56:35 -0400

    Final Push

--- src/App.js
+++ src/App.js
@@ -1,8 +1,9 @@
 import React, { Component } from "react";
-//import "./App.css";
+import "./App.css";
 import Nasa from "./Nasa";
 import OpenWeather from "./OpenWeather";
 import Zomato from "./Zomato";
+import { Container, Grid } from "@material-ui/core";
 
 class App extends Component {
   constructor(props) {
@@ -31,93 +32,98 @@ class App extends Component {
     let zData = this;
     let zomatoKey = "0586265890c1a72bf88272b30187f388";
     let wData = this;
-    let weatherKey = 'ac20f50ab1f3942a55f9aa45dab00f26';
+    let weatherKey = "ac20f50ab1f3942a55f9aa45dab00f26";
 
-    
     // **** Get the longitude & latitude for the other API calls. ****
 
     navigator.geolocation.getCurrentPosition(function (position) {
       console.log("latitude is:", position.coords.latitude);
       console.log("longitude is:", position.coords.longitude);
-      location.setState ({
+      location.setState({
         latitude: position.coords.latitude,
         longitude: position.coords.longitude,
       });
-      console.log('**' + position.coords.latitude);
+      console.log("**" + position.coords.latitude);
       console.log(location);
-
-
 
       // **** Zomato API Call ****
 
-      let zomatoURL = `https://developers.zomato.com/api/v2.1/geocode?lat=${location.state.latitude}&lon=${location.state.longitude}`
-      console.log('Zomato URL = ' + zomatoURL);
-  
-      fetch (zomatoURL, {
-        method: 'GET',
-        headers: {  
-            'Accept': 'application/json',
-            'User-Key': `${zomatoKey}`
-        }
+      let zomatoURL = `https://developers.zomato.com/api/v2.1/geocode?lat=${location.state.latitude}&lon=${location.state.longitude}`;
+      console.log("Zomato URL = " + zomatoURL);
+
+      fetch(zomatoURL, {
+        method: "GET",
+        headers: {
+          Accept: "application/json",
+          "User-Key": `${zomatoKey}`,
+        },
       })
-        .then((res) => 
-            res.json()
-        )
+        .then((res) => res.json())
         .then((data) => {
-            console.log('Zomato API Result Data');
-            console.log(data);
-            zData.setState ({
-              zomatoRest1Name: data.nearby_restaurants[0].restaurant.name,
-              zomatoRest2Name: data.nearby_restaurants[1].restaurant.name,
-              zomatoRest3Name: data.nearby_restaurants[2].restaurant.name,
-              zomatoRest1PhotosURL: data.nearby_restaurants[0].restaurant.thumb,
-              zomatoRest2PhotosURL: data.nearby_restaurants[1].restaurant.thumb,
-              zomatoRest3PhotosURL: data.nearby_restaurants[2].restaurant.thumb,
-            });
-      
-        });
-      
-
-
-        // **** Open Weather API Call ****
-        let weatherURL = `https://api.openweathermap.org/data/2.5/weather?lat=${location.state.latitude}&lon=${location.state.longitude}&appid=${weatherKey}`
-        console.log('weatherURL = ' + weatherURL);
-
-        fetch(weatherURL, {
-          method: 'GET',
-          headers: {
-              'Accept': 'application.json',
-          }
-        })
-        .then((res) =>
-          res.json()
-        )
-        .then((data) => {
-          console.log('Weather Data');
+          console.log("Zomato API Result Data");
           console.log(data);
-          wData.setState ({
-            weatherDescription: data.weather[0].description,
-            weatherTemp: data.main.temp
+          zData.setState({
+            zomatoRest1Name: data.nearby_restaurants[0].restaurant.name,
+            zomatoRest2Name: data.nearby_restaurants[1].restaurant.name,
+            zomatoRest3Name: data.nearby_restaurants[2].restaurant.name,
+            zomatoRest1PhotosURL: data.nearby_restaurants[0].restaurant.thumb,
+            zomatoRest2PhotosURL: data.nearby_restaurants[1].restaurant.thumb,
+            zomatoRest3PhotosURL: data.nearby_restaurants[2].restaurant.thumb,
           });
         });
 
-        
-    });  // End of navigator.geolocation.getCurrentPosition
-    
+      // **** Open Weather API Call ****
+      let weatherURL = `https://api.openweathermap.org/data/2.5/weather?lat=${location.state.latitude}&lon=${location.state.longitude}&appid=${weatherKey}`;
+      console.log("weatherURL = " + weatherURL);
 
+      fetch(weatherURL, {
+        method: "GET",
+        headers: {
+          Accept: "application.json",
+        },
+      })
+        .then((res) => res.json())
+        .then((data) => {
+          console.log("Weather Data");
+          console.log(data);
+          wData.setState({
+            weatherDescription: data.weather[0].description,
+            weatherTemp: data.main.temp,
+          });
+        });
+    }); // End of navigator.geolocation.getCurrentPosition
   }
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <h1>
-            Based on your location, here is some vital information for you!
-          </h1>
-        </header>
-        <Nasa latitude={this.state.latitude} longitude={this.state.longitude} />
-        <OpenWeather weatherDescription={this.state.weatherDescription} temp={this.state.weatherTemp} />
-        <Zomato zomatoRest1Name={this.state.zomatoRest1Name} zomatoRest2Name={this.state.zomatoRest2Name} zomatoRest3Name={this.state.zomatoRest3Name} zomatoRest1PhotosURL={this.state.zomatoRest1PhotosURL} zomatoRest2PhotosURL={this.state.zomatoRest2PhotosURL} zomatoRest3PhotosURL={this.state.zomatoRest3PhotosURL} />
-      </div>
+      <Container>
+        <div className="mainDiv">
+          <Grid item xs container direction="column" alignItems="center">
+            <header className="App-header">
+              <h1>
+                Based on your location, here is some vital information for you!
+              </h1>
+            </header>
+            <Grid item alignItems="center">
+              <Nasa
+                latitude={this.state.latitude}
+                longitude={this.state.longitude}
+              />
+            </Grid>
+            <OpenWeather
+              weatherDescription={this.state.weatherDescription}
+              temp={this.state.weatherTemp}
+            />
+            <Zomato
+              zomatoRest1Name={this.state.zomatoRest1Name}
+              zomatoRest2Name={this.state.zomatoRest2Name}
+              zomatoRest3Name={this.state.zomatoRest3Name}
+              zomatoRest1PhotosURL={this.state.zomatoRest1PhotosURL}
+              zomatoRest2PhotosURL={this.state.zomatoRest2PhotosURL}
+              zomatoRest3PhotosURL={this.state.zomatoRest3PhotosURL}
+            />
+          </Grid>
+        </div>
+      </Container>
     );
   }
 }
--- src/OpenWeather.js
+++ src/OpenWeather.js
@@ -3,12 +3,13 @@ import { makeStyles } from "@material-ui/core/styles";
 import Card from "@material-ui/core/Card";
 import CardActionArea from "@material-ui/core/CardActionArea";
 import CardContent from "@material-ui/core/CardContent";
-import CardMedia from "@material-ui/core/CardMedia";
 import Typography from "@material-ui/core/Typography";
-
+import HUE from "@material-ui/core/colors/yellow";
+const colorYellow = HUE[300];
 const useStyles = makeStyles({
   root: {
     maxWidth: 345,
+    backgroundColor: colorYellow,
   },
   media: {
     height: 140,
@@ -18,56 +19,40 @@ const useStyles = makeStyles({
 export default function OpenWeather(props) {
   const { temp } = props;
   const { weatherDescription } = props;
-  // const { fahr } = props;
-  // const { handleClick } = props;
   const [fahr, setFahr] = useState(true);
-  // const [fahr, setFahr] = useState(true);
-  // this.state = {
-  //     fahr: true
-  // }
 
-  // const { fahr } = props;
-  // var fahr = true;
-  // console.log(fahr)
   const classes = useStyles();
 
   const handleClick = () => {
     setFahr(!fahr);
-    // console.log(fahr);
-    // return !fahr;
-    // fahr = !fahr;
-    // console.log(fahr);
-    // return fahr;
-    // this.setState ({fahr: !fahr})
   };
 
   // Temperature Variables
 
   let fTemp = (temp - 273) * 1.8 + 32;
-  let fTempFixed = fTemp.toFixed(2);
+  let fTempFixed = fTemp.toFixed(0);
   let cTemp = temp - 273;
-  let cTempFixed = cTemp.toFixed(2);
+  let cTempFixed = cTemp.toFixed(0);
 
   return (
     <div>
       <h2>Your Weather</h2>
       <Card className={classes.root}>
-        {/* <CardActionArea> */}
-        {/* <CardActionArea onClick={(fahr) => handleClick(fahr)}> */}
         <CardActionArea onClick={() => setFahr(!fahr)}>
           <CardContent>
-            <Typography gutterBottom variant="h5" component="h2">
-              Current Weather: {weatherDescription}
+            <Typography variant="h5" component="h3">
+              Current Weather:
+            </Typography>
+            <Typography gutterBottom variant="h7" component="h3">
+              {weatherDescription}
             </Typography>
             {fahr ? (
               <Typography variant="body2" color="textSecondary" component="p">
-                {/* Fahrenheit */}
-                Current Temperature: {fTempFixed} F
+                Temp: <strong>{fTempFixed} F </strong>
               </Typography>
             ) : (
               <Typography variant="body2" color="textSecondary" component="p">
-                {/* Celcius */}
-                Current Temperature: {cTempFixed} C
+                Temp: <strong>{cTempFixed} C</strong>
               </Typography>
             )}
           </CardContent>
